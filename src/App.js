bd6702151f42c874ae9adcaaf842a2c3c90a5b0c
karl ducta <karlducta022@gmail.com>
2023-10-30 21:52:02 +0800

    --- Rendering the Items list --- Building Forms and Handling Submissions ---

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+];
+
 const App = () => {
   return (
     <div className="app">
@@ -14,15 +19,49 @@ const Logo = () => {
 };
 
 const Form = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault(); // default behavior (reload) of form submission
+  };
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip?</h3>
-    </div>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="item..." />
+      <button>Add</button>
+    </form>
   );
 };
 const PackageList = () => {
-  return <div className="list">List</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
 };
+
+const Item = ({ item }) => {
+  return (
+    <li>
+      {/* <input type="checkbox" /> */}
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity}x {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+};
+
 const Stats = () => {
   return (
     <footer className="stats">
